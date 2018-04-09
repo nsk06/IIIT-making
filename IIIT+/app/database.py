e393f8175ac62a436f591d143050d2cce7435969
nsk06 <32328815+nsk06@users.noreply.github.com>
2018-04-10 03:06:34 +0530

    Add files via upload
    new added features

--- IIIT+/app/database.py
+++ IIIT+/app/database.py
@@ -1,9 +1,9 @@
-from views import db
+from views import db,login
 from datetime import datetime
 from passlib.apps import custom_app_context as pwd_context
 from passlib.context import CryptContext
-#from flask_login import *
-#import login
+from flask_login import UserMixin
+#from login import Login
 pwd_context = CryptContext(
 
     schemes=["pbkdf2_sha512"],
@@ -16,15 +16,19 @@ class User(UserMixin,db.Model):
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
+    posts = db.relationship('Post', backref='author', lazy='dynamic')
 
+    def __repr__(self):
+        return '<User {}>'.format(self.username)
     def set_password(self, password):
         self.password_hash = pwd_context.encrypt(password)
 
     def check_password(self, password):
         return pwd_context.verify(password,self.password_hash)
 
-    def __repr__(self):
-        return '<User {}>'.format(self.username)
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     body = db.Column(db.String(140))
--- IIIT+/app/views.py
+++ IIIT+/app/views.py
@@ -1,20 +1,25 @@
 from flask import *
 from flask_bootstrap import Bootstrap
 from Key import Config
-from login import Login
+from werkzeug.urls import url_parse
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-from flask_login import *
-from database import User
+from flask_login import login_user, logout_user, current_user
+from flask_login import login_required,LoginManager
+
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
-from database import 
 login = LoginManager(app)
+login.login_view = 'login'
+from database import User
+from login import *
+
 @app.route('/')
 @app.route('/index')
+@login_required
 def index():
     user = {'username': 'Nonidh'}
     posts = [
@@ -29,9 +34,6 @@ def index():
     ]
     return render_template('index.html', title='Home', user=user, posts=posts)
 
-@login.user_loader
-def load_user(id):
-    return User.query.get(int(id))
 @app.route('/login',methods  = ['GET', 'POST'])
 def login():
     if current_user.is_authenticated:
@@ -43,12 +45,32 @@ def login():
             flash('Invalid username or password')
             return redirect(url_for('login'))
         login_user(user, remember=form.remember_me.data)
-        return redirect(url_for('index'))
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('index')
+        return redirect(next_page)
     return render_template('login.html', title='Sign In', form=form)
+
+
 @app.route('/logout')
 def logout():
     logout_user()
     return redirect(url_for('index'))
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Congratulations, you are now a registered user!')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)
+
 
 @app.shell_context_processor
 def make_shell_context():
