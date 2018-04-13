de7b2e4f338d04cdb9c532a187db94ee077a68c2
nsk06 <32328815+nsk06@users.noreply.github.com>
2018-04-13 06:36:32 +0530

    Add files via upload
    pic upload,posts upload

--- IIIT+/app/database.py
+++ IIIT+/app/database.py
@@ -11,6 +11,13 @@ pwd_context = CryptContext(
     all__vary_rounds = 0.2,
     pbkdf2_sha512__default_rounds = 8000,
     )
+
+followers = db.Table('followers',
+    db.Column('follower_id', db.Integer, db.ForeignKey('user.id')),
+    db.Column('followed_id', db.Integer, db.ForeignKey('user.id'))
+)
+
+
 class User(UserMixin,db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
@@ -19,6 +26,13 @@ class User(UserMixin,db.Model):
     posts = db.relationship('Post', backref='author', lazy='dynamic')
     about_me = db.Column(db.String(140))
     last_seen = db.Column(db.DateTime, default=datetime.utcnow)
+    
+    followed = db.relationship(
+        'User', secondary=followers,
+        primaryjoin=(followers.c.follower_id == id),
+        secondaryjoin=(followers.c.followed_id == id),
+        backref=db.backref('followers', lazy='dynamic'), lazy='dynamic')
+
     def __repr__(self):
         return '<User {}>'.format(self.username)
     def set_password(self, password):
@@ -26,6 +40,24 @@ class User(UserMixin,db.Model):
 
     def check_password(self, password):
         return pwd_context.verify(password,self.password_hash)
+    def follow(self, user):
+        if not self.is_following(user):
+            self.followed.append(user)
+
+    def unfollow(self, user):
+        if self.is_following(user):
+            self.followed.remove(user)
+
+    def is_following(self, user):
+        return self.followed.filter(
+            followers.c.followed_id == user.id).count() > 0
+
+    def followed_posts(self):
+        followed = Post.query.join(
+            followers, (followers.c.followed_id == Post.user_id)).filter(
+                followers.c.follower_id == self.id)
+        own = Post.query.filter_by(user_id=self.id)
+        return followed.union(own).order_by(Post.timestamp.desc())    
 
 @log.user_loader
 def load_user(id):
